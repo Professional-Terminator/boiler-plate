d87f859bc90d26decac6f8b662a1aac01f30fe15
SaqibOctave <sali@octavebytes.com>
2023-12-27 17:18:33 +0500

    send

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const routes = require("./routes/routes")
 const app = express();
 const port = 3000;
 app.use(express.json())
+// checking
 app.use("/api", routes)
 app.listen(port, ()=>{console.log(`Server listening on port ${port}`)})
